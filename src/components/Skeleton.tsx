5bc3c4bce8995b66c5062e4aadd5b9931bba2c86
nanqic <nanqic@163.com>
2024-03-26 23:11:08 +0800

    opt: skeleton height

--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -2,9 +2,9 @@
 export default function Skeleton() {
     return (
         <div className="flex flex-col gap-4 px-3">
-            <div className="skeleton h-5 w-full"></div>
-            <div className="skeleton h-5 w-full"></div>
-            <div className="skeleton h-5 w-full"></div>
+            <div className="skeleton h-36 w-full"></div>
+            <div className="skeleton h-36 w-full"></div>
+            <div className="skeleton h-36 w-full"></div>
         </div>
     )
 }
