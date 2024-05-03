afda9600f445783425b5338ac6e6197cce04b33b
nanqic <nanqic@163.com>
2024-05-03 13:44:25 +0800

    opt: footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,9 @@
 export default function Footer() {
     return (
-        <footer className="flex flex-col items-center justify-center gap-2 p-4 bg-neutral text-neutral-content">
+        <footer className="flex flex-col items-center justify-center gap-2 p-4 bg-white text-white-content">
             <nav className="space-x-4">
-                <a target='_blank' href="https://dev.hdcx.pages.dev">慧灯禅修 dev</a>
-                <a target='_blank' href="https://cxbbj.huidengchanxiu.net">禅修班笔记 dev</a>
+                <a target='_blank' href="https://www.huidengchanxiu.net">慧灯禅修</a>
+                <a target='_blank' href="https://cxbbj.huidengchanxiu.net">禅修班笔记</a>
                 <a target='_blank' href="https://www.fahaisouxun.com/search">法海搜寻</a>
                 <a target='_blank' href="https://a.hdcxb.net/login2">有声书</a>
             </nav>
